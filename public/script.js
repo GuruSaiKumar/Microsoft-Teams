14db79b8b767b11b8539dab7fcd2cd5640af3229
GuruSaiKumar <gurusaikumar2002@gmail.com>
2021-06-30 13:25:11 +0530

    changed script.js for hosting

--- public/script.js
+++ public/script.js
@@ -23,9 +23,9 @@ const user = prompt("Enter your name");
 
 var peer = new Peer(undefined, {
   // path: "/peerjs",
-  host: "/",//While deploying host may change
-  port: "8081",//cannot be less than some 1000s
-  secure: false // For checking secure or insecure http or https
+  host: "https://microsoft-teams-clone-guru.herokuapp.com/",//While deploying host may change
+  port: "443",//cannot be less than some 1000s
+  secure: true // For checking secure or insecure http or https
   //peerjs --port 8081  
 });
 
