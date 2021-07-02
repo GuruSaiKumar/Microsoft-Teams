0eb43452b7c1ec4ad9081823797954c21dd2850a
GuruSaiKumar <gurusaikumar2002@gmail.com>
2021-07-02 16:24:51 +0530

    Added my own peerjs server

--- public/script.js
+++ public/script.js
@@ -27,7 +27,7 @@ const user = prompt("Enter your name");
 
 var peer = new Peer(undefined, {
   // path: "peerjs-server.herokuapp.com",
-  host: "https://microsoft-teams-clone-guru.herokuapp.com/",//While deploying host may change
+  host: "https://gurus-peerjs-server.herokuapp.com/",//While deploying host may change
   // host: '/',
   port: 443,//cannot be less than some 1000s
   secure: true // For checking secure or insecure http or https
